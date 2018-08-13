3e18ab326c7cada82c4247cd4622bf6f49f1e33b
Raman Khurana <ramankhurana1986@gmail.com>
2018-08-14 00:20:20 +0200

    creating remove dead rootfiles

---
+++ RemoveDeadRootFiles.sh
@@ -0,0 +1,13 @@
+## in this script change the name of rootfile and treename 
+## and run using source RemoveDeadRootFiles.sh
+## you should see the number of entries in the tree 
+## test this on a dead file and then I can follow up from that point onwards, how to edit the script. 
+
+rootfilename=mlfit.root
+treename=tree_fit_sb
+
+root -l <<EOF                                                                                                                                                                        
+TFile* f = new TFile("$rootfilename","READ");                                                                                                                                        
+f->cd();                                                                                                                                                                             
+${treename}->GetEntries()                                                                                                                                                            
+EOF                                                                                                                                                                                  
