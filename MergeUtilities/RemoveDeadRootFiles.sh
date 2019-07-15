405a5a07cd0b6d9f3480e7d69ab22186cecbd58f
Raman Khurana <ramankhurana1986@gmail.com>
2019-07-15 13:11:54 +0200

    update removedeadfile.sh
    works fine now, just change the dir name and tree name

--- MergeUtilities/RemoveDeadRootFiles.sh
+++ MergeUtilities/RemoveDeadRootFiles.sh
@@ -3,13 +3,25 @@
 ## you should see the number of entries in the tree 
 ## test this on a dead file and then I can follow up from that point onwards, how to edit the script. 
 
-dirname="path_to_rootfiles_"
-rootfilename=mlfit.root
-treename=tree_fit_sb
+dirname="/eos/cms/store/user/khurana//ExoPieInputs/MergedSkims/Merged_tDM_06052019/"
 
-root -l <<EOF                                                                                                                                                                        
-TFile* f = new TFile("$rootfilename","READ");                                                                                                                                        
-f->cd();                                                                                                                                                                             
-echo ${treename}->GetEntries()     
+ls -1 $dirname > files.txt
+filelist=`cat files.txt`
+treename='outTree'
 
-EOF                                                                                                                                                                                  
+
+for which in  $filelist  ; do
+
+    rootfilename=$dirname/$which
+    echo $which
+    root -l <<EOF                                                                                                                                                                                           
+                                                                                                                                                                                                            
+    TFile* f = new TFile("$rootfilename","READ");                                                                                                                                                           
+    f->cd();                                                                                                                                                                                                
+                                                                                                                                                                                                            
+     ${treename}->GetEntries()                                                                                                                                                                              
+EOF                                                                                                                                                                                                         
+
+
+done
+
